f367b0a9bfa0fbd80dab3713a66caadea454ac6b
misha31415 <ivanov21314@gmail.com>
2017-11-02 23:17:59 +0300

    2.2 исправил круг

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -40,7 +40,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = ((Math.abs(x2 
  */
 
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = ((sqr(x2 - x1) + sqr(y2 - y1)) + r1 <= r2)
+                 x2: Double, y2: Double, r2: Double): Boolean = (Math.sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1 <= r2)
 
 /**
  * Средняя
